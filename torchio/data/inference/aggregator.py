308c22532488c3a77c72ecda330854264db0f047
Fernando <fepegar@gmail.com>
2020-06-05 16:11:47 +0100

    Improve readability

--- torchio/data/inference/aggregator.py
+++ torchio/data/inference/aggregator.py
@@ -36,51 +36,43 @@ class GridAggregator:
 
     @staticmethod
     def _crop_batch(
-            patches: torch.Tensor,
+            batch: torch.Tensor,
             location: np.ndarray,
             border: Tuple[int, int, int],
             ) -> Tuple[TypeData, np.ndarray]:
-        location = location.astype(np.int)
-        batch_shape = patches.shape
+        crop_location = location.astype(np.int).copy()
+        batch_shape = batch.shape
         spatial_shape = batch_shape[2:]  # ignore batch and channels dim
         num_dimensions = 3
         for idx in range(num_dimensions):
-            location[:, idx] = location[:, idx] + border[idx]
-            location[:, idx + 3] = location[:, idx + 3] - border[idx]
-        cropped_shape = np.max(location[:, 3:6] - location[:, 0:3], axis=0)
+            crop_location[:, idx] = crop_location[:, idx] + border[idx]
+            crop_location[:, idx + 3] = crop_location[:, idx + 3] - border[idx]
+        cropped_shape = np.max(crop_location[:, 3:6] - crop_location[:, 0:3], axis=0)
         diff = spatial_shape - cropped_shape
         left = np.floor(diff / 2).astype(np.int)
         i_ini, j_ini, k_ini = left
         i_fin, j_fin, k_fin = left + cropped_shape
-        batch = patches[
-            :,  # batch dimension
-            :,  # channels dimension
-            i_ini:i_fin,
-            j_ini:j_fin,
-            k_ini:k_fin,
-        ]
-        return batch, location
+        cropped_batch = batch[..., i_ini:i_fin, j_ini:j_fin, k_ini:k_fin]
+        return cropped_batch, crop_location
 
     def _ensure_output_dtype(self, tensor: torch.Tensor) -> None:
         """Make sure the output tensor type is the same as the input patches."""
         if self._output_tensor.dtype != tensor.dtype:
             self._output_tensor = self._output_tensor.type(tensor.dtype)
 
-    def add_batch(self, patches: torch.Tensor, locations: TypeData) -> None:
-        patches = patches.cpu()
-        self._ensure_output_dtype(patches)
-        location_init = np.copy(locations)
-        init_ones = np.ones_like(patches)
-        patches, _ = self._crop_batch(
-            patches, location_init, self.patch_overlap)
-        location_init = np.copy(locations)
-        _, locations = self._crop_batch(
-            init_ones, location_init, self.patch_overlap)
-        for patch, location in zip(patches, locations):
+    def add_batch(self, batch: torch.Tensor, locations: TypeData) -> None:
+        batch = batch.cpu()
+        locations = locations.cpu().numpy()
+        self._ensure_output_dtype(batch)
+        cropped_batch, crop_locations = self._crop_batch(batch, locations, self.patch_overlap)
+        for patch, location in zip(cropped_batch, crop_locations):
             i_ini, j_ini, k_ini, i_fin, j_fin, k_fin = location
-            channels = len(patch)
-            for channel in range(channels):
-                self._output_tensor[channel, i_ini:i_fin, j_ini:j_fin, k_ini:k_fin] = patch[channel]
+            for channel_idx, tensor in enumerate(patch):
+                self._output_tensor[
+                    channel_idx,
+                    i_ini:i_fin,
+                    j_ini:j_fin,
+                    k_ini:k_fin] = tensor
 
     def get_output_tensor(self) -> torch.Tensor:
         return self._output_tensor
